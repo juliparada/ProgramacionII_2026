f81341ae8a7b591b6b9efb5b00568767edd21c7d
Yeimy Chávez <yeimyjulissa006@gmail.com>
2026-02-25 20:24:24 -0600

    Se agrego la funcionalidad de la calculadora basica

--- app/src/main/java/com/example/myaplicacion/MainActivity.java
+++ app/src/main/java/com/example/myaplicacion/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myaplicacion;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,27 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tempVal;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        btn = findViewById(R.id.btnCalcular);
+        btn.setOnClickListener(v-> calcular());
+    }
+    private void calcular(){
+        tempVal = findViewById(R.id.txtNum1);
+        Double num1 = Double.parseDouble(tempVal.getText().toString());
+
+        tempVal = findViewById(R.id.txtNum2);
+        Double num2 = Double.parseDouble(tempVal.getText().toString());
+
+        double respuesta = num1 + num2;
+
+        tempVal = findViewById(R.id.lblRespuesta);
+        tempVal.setText("Respuesta: "+ respuesta);
     }
 }
